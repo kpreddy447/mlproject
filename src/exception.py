61d54f34836b22c45daeffac8743ff6e12d60a58
Preethika Reddy Karra <kpreethi447@gmail.com>
2024-10-07 18:42:44 -0400

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,4 @@
-#for exceptional handling purpouse
-import sys 
+import sys
 from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
@@ -7,10 +6,7 @@ def error_message_detail(error,error_detail:sys):
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
      file_name,exc_tb.tb_lineno,str(error))
-
     return error_message
-
-    
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
@@ -19,3 +15,4 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+
